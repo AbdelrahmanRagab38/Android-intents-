bee5b64d179513feafc0761f64af6b8e679d204a
AbdelrahmanRagab38 <abdelrahmanRagab38@gmail.com>
2019-12-24 06:21:57 -1000

    first commit

---
+++ app/src/main/java/com/example/dell/implicitintents/FirstActivity.java
@@ -0,0 +1,138 @@
+package com.example.dell.implicitintents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.android.implicitintents.R;
+
+import java.util.Locale;
+
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+
+    /**
+     * This method is called when the Open Website button is clicked. It will open the website
+     * specified by the URL represented by the variable urlAsString using implicit Intents.
+     *
+     * @param v Button that was clicked.
+     */
+    public void onClickOpenWebpageButton(View v) {
+        String urlAsString = "http://www.google.com";
+        openWebPage(urlAsString);
+    }
+
+    /**
+     * This method is called when the Open Location in Map button is clicked. It will open the
+     * a map to the location represented by the variable addressString using implicit Intents.
+     *
+     * @param v Button that was clicked.
+     */
+    public void onClickOpenAddressButton(View v) {
+        // TODO (5) Store an address in a String
+String myAdress="Maadi";
+        // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
+Uri.Builder builder=new Uri.Builder();
+builder.scheme("geo")
+        .path("0,0")
+        .query(myAdress);
+Uri myadressUri= builder.build();
+        // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
+        showMap(myadressUri);
+    }
+
+    /**
+     * This method is called when the Share Text Content button is clicked. It will simply share
+     * the text contained within the String textThatYouWantToShare.
+     *
+     * @param v Button that was clicked.
+     */
+    public void onClickShareTextButton(View v) {
+        // TODO (5) Specify a String you'd like to share
+String myString="world is not safe my baby";
+        // TODO (6) Replace the Toast with shareText, passing in the String from step 5
+shareText(myString);    }
+
+    /**
+     * This is where you will create and fire off your own implicit Intent. Yours will be very
+     * similar to what I've done above. You can view a list of implicit Intents on the Common
+     * Intents page from the developer documentation.
+     *
+     * @see <http://developer.android.com/guide/components/intents-common.html/>
+     *
+     * @param v Button that was clicked.
+     */
+    public void createYourOwn(View v) {
+        Toast.makeText(this,
+                "TODO: Create Your Own Implicit Intent",
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    /**
+     * This method fires off an implicit Intent to open a webpage.
+     *
+     * @param url Url of webpage to open. Should start with http:// or https:// as that is the
+     *            scheme of the URI expected with this Intent according to the Common Intents page
+     */
+    private void openWebPage(String url) {
+        /*
+         * We wanted to demonstrate the Uri.parse method because its usage occurs frequently. You
+         * could have just as easily passed in a Uri as the parameter of this method.
+         */
+        Uri webpage = Uri.parse(url);
+
+        /*
+         * Here, we create the Intent with the action of ACTION_VIEW. This action allows the user
+         * to view particular content. In this case, our webpage URL.
+         */
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        /*
+         * This is a check we perform with every implicit Intent that we launch. In some cases,
+         * the device where this code is running might not have an Activity to perform the action
+         * with the data we've specified. Without this check, in those cases your app would crash.
+         */
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
+    // TODO (1) Create a method called showMap with a Uri as the single parameter
+    private void  showMap(Uri uri){
+    // Do steps 2 - 4 within the showMap method
+    // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
+Intent intent=new Intent(Intent.ACTION_VIEW);
+    // TODO (3) Set the data of the Intent to the Uri passed into this method
+intent.setData(uri);
+    // TODO (4) Verify that this Intent can be launched and then call startActivity
+if (intent.resolveActivity(getPackageManager())!=null){
+startActivity(intent);}
+    }
+    // TODO (1) Create a void method called shareText that accepts a String as a parameter
+    // Do steps 2 - 4 within the shareText method
+    private  void shareText(String text){
+
+    // TODO (2) Create a String variable called mimeType and set it to "text/plain"
+String mimetype="text/plain";
+    // TODO (3) Create a title for the chooser window that will pop up
+String title="Abdelrahman Ragab";
+    // TODO (4) Use ShareCompat.IntentBuilder to build the Intent and start the chooser
+        ShareCompat.IntentBuilder.from(this)
+                .setChooserTitle(title)
+                .setType(mimetype)
+        .setText(text);
+        }
+    }
+
